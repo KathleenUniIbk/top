2a6acc48805ed56901b3d7cb0ea93c6d6d8a0e4b
KathleenDelp <delpk@students.uni-marburg.de>
2025-03-29 13:31:20 +0100

    Backticks für mehrzeiliges Popup

--- main.js
+++ main.js
@@ -15,4 +15,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 let marker = L.marker([60.383333, 5.383333]).addTo(map);
 
 //Popup definieren und öffnen
-marker.bindPopup("<b>Hello world!</b><br>I am Ulriken.").openPopup();
+marker.bindPopup(`
+    <b>Hello world!</b>
+    <br>I am Ulriken.
+    `).openPopup();
