87535c58a002e370d20d09246f8d685e3c9f4219
KathleenDelp <delpk@students.uni-marburg.de>
2025-03-29 13:27:31 +0100

    var zu let ersätzt

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ Skript für Lieblingsorte Webmapping
 */
 
 //Karte initalisieren
-var map = L.map('map').setView([60.383333, 5.383333], 13);
+let map = L.map('map').setView([60.383333, 5.383333], 13);
 
 // Hintergrundkarte definieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -12,7 +12,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
  //Marker zeichnen
-var marker = L.marker([60.383333, 5.383333]).addTo(map);
+let marker = L.marker([60.383333, 5.383333]).addTo(map);
 
 //Popup definieren und öffnen
 marker.bindPopup("<b>Hello world!</b><br>I am Ulriken.").openPopup();
